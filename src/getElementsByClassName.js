03c20364f5a1b43af5322488eebbfa4ba8993dc5
Derek Wu <Derek@Dereks-MacBook-Air.local>
2014-09-14 14:41:34 -0700

    finished get elementsByClassName()

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,28 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  return document.body;
+  var body = document.body.childNodes;
+  var result = [];
+
+  	if(document.body.classList.contains(className)){
+	  	result.push(document.body);  	
+	 };
+
+  var recurse = function(collections){
+
+	  for(var i = 0; i <collections.length;i++){
+	  	var collection = collections[i];
+	  	var children = collection.childNodes;
+	  	var classList = collection.classList;
+	  	if(classList && classList.contains(className)){
+		  	result.push(collection);  	
+		 };
+	  	if(children){
+	  		recurse(children);
+	  	};
+	  }
+  };
+
+  recurse(body);
+  return result;
 };
